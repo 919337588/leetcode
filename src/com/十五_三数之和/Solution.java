65f5c66ad71a5bcfb69cf74895f86febad2418b5
chixin <chixin01@renmaitech.com>
2019-12-17 20:59:33 +0800

    Default Changelist

---
+++ src/com/十五_三数之和/Solution.java
@@ -0,0 +1,13 @@
+package com.十五_三数之和;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashMap<Integer ,HashMap<Integer,Integer>> hashMap=new HashMap<>();
+
+        return null;
+    }
+}
