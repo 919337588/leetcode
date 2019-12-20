add675adc3cbbc2ba68fde519e36b28666e24be5
chixin <chixin01@renmaitech.com>
2019-12-20 10:39:28 +0800

    Default Changelist

--- src/com/十五_三数之和/Solution.java
+++ src/com/十五_三数之和/Solution.java
@@ -1,13 +1,47 @@
 package com.十五_三数之和;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        HashMap<Integer ,HashMap<Integer,Integer>> hashMap=new HashMap<>();
-
-        return null;
+    public static void main(String[] args) {
+        System.out.println(new Solution().threeSum(new int[]{3,0,-2,-1,1,2}));
     }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> re=new LinkedList<>();
+        HashMap<Integer,HashMap<Integer,Boolean>> qc=new HashMap<>();
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map=new HashMap();
+        for (int i = 0; i <nums.length ; i++) {
+            map.put(nums[i],i);
+        }
+        for (int i = 1; i <nums.length-1 ; i++) {
+            for(int j=0;j<i;j++){
+                if(j>0&&nums[j]==nums[j-1]){
+                    continue;
+                }
+                Integer need=-1*(nums[j]+nums[i]);
+               Integer set=map.get(need);
+                if(set!=null&&set>i){
+                    HashMap<Integer,Boolean> integers = qc.get(nums[j]);
+                    if(integers==null){
+                        re.add(Arrays.asList(nums[j],nums[i],need));
+                        integers=new HashMap<>();
+                        integers.put(nums[i],true);
+                        qc.put(nums[j],integers);
+                    }else if(!integers.containsKey(nums[i])){
+                        re.add(Arrays.asList(nums[j],nums[i],need));
+                        integers.put(nums[i],true);
+                    }
+                }
+            }
+        }
+        return re;
+    }
+
 }
