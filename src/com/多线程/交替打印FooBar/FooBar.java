9345bb5ec12dc54e15ffab04e29aefa349c3f3db
chixin <chixin01@renmaitech.com>
2020-04-13 11:07:43 +0800

    Default Changelist

--- src/com/多线程/交替打印FooBar/FooBar.java
+++ src/com/多线程/交替打印FooBar/FooBar.java
@@ -1,8 +1,15 @@
 package com.多线程.交替打印FooBar;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class FooBar {
     private int n;
-
+    Lock lock=new ReentrantLock();
+    Condition condition1 = lock.newCondition();
+    Condition condition2 = lock.newCondition();
+    volatile int clout =1;
     public FooBar(int n) {
         this.n = n;
     }
@@ -10,18 +17,31 @@ class FooBar {
     public void foo(Runnable printFoo) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
-        	printFoo.run();
+            lock.lock();
+            if(clout!=1){
+                condition1.await();
+            }
+            printFoo.run();
+            clout=2;
+            condition2.signalAll();
+            lock.unlock();
+
+
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            
-            // printBar.run() outputs "bar". Do not change or remove this line.
+            lock.lock();
+            if(clout!=2){
+                condition2.await();
+            }
         	printBar.run();
+            clout=1;
+            condition1.signalAll();
+            lock.unlock();
+
         }
     }
 }
