be2993c5b7a6e6c3e82e312048f76a0c60f00d69
chixin <chixin@zk01.cn>
2020-04-17 15:54:48 +0800

    Default Changelist

--- src/com/剑指offer/重建二叉树/Solution.java
+++ src/com/剑指offer/重建二叉树/Solution.java
@@ -1,5 +1,7 @@
 package com.剑指offer.重建二叉树;
 
+import java.util.Arrays;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -10,10 +12,36 @@ package com.剑指offer.重建二叉树;
  * }
  */
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public static void main(String[] args) {
+        TreeNode treeNode = new Solution().buildTree(new int[]{1,2}, new int[]{2,1});
 
+        System.out.println(treeNode);
+    }
+    int[] preorder, inorder;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder=preorder;
+        this.inorder=inorder;
+        if(preorder.length==0 || inorder.length==0) {
+            return null;
+        }
+       return build(0,preorder.length-1,0,inorder.length-1);
     }
 
+    public TreeNode build(int pb,int po,int ib,int io){
+        if(pb>po || ib>io) {
+            return null;
+        }
+        TreeNode treeNode=new TreeNode(preorder[pb]);
+        for (int i = ib; i <=io ; i++) {
+            if(inorder[i]==preorder[pb]){
+                int j=i-ib+pb;
+                treeNode.left=build(pb+1,j,ib,i-1);
+                treeNode.right=build(j+1,po,i+1,io);
+
+            }
+        }
+        return treeNode;
+    }
     public class TreeNode {
         int val;
         TreeNode left;
